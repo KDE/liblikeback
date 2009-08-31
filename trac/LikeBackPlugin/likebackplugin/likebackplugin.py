4b248e8aa470d6bc203bed3489c689ed85dec28a
Valerio Pilo <valerio@kmess.org>
2009-08-31 12:31:36 +0000

    likeback:  * Added error reporting to the Trac plugin;  * moved its configuration to trac.ini;  * rewritten the README.

--- trac/LikeBackPlugin/likebackplugin/likebackplugin.py
+++ trac/LikeBackPlugin/likebackplugin/likebackplugin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 LikeBackPlugin:
 a plugin for Trac
@@ -9,10 +10,8 @@ from trac.core import *
 from trac.ticket.api import ITicketChangeListener
 import urllib
 import urllib2
+from urllib2 import HTTPError
 
-# alter these yourself:
-likeback_url = "http://kmess.org/likeback/backend/trac_signal.php"
-integration_secret = ""
 
 class LikeBackPlugin(Component):
 
@@ -25,11 +24,20 @@ class LikeBackPlugin(Component):
 
     def ticket_changed(self, ticket, comment, author, old_values):
         """Called when a ticket is modified.
-        
+
         `old_values` is a dictionary containing the previous values of the
         fields that have changed.
         Notify LikeBack, it will handle the rest.
         """
+
+        # First, retrieve the configuration options we will need.
+        integration_secret = self.config.get( 'likeback-plugin', 'integration_secret' ) or self._die( 'no_conf', 'integration_secret' )
+        likeback_url = self.config.get( 'likeback-plugin', 'likeback_url' ) or self._die( 'no_conf', 'likeback_url' )
+
+        # Append the LikeBack Trac Signal script name to the URL
+        likeback_url = likeback_url + 'trac_signal.php'
+
+        # Fill up the list of changed ticket details
         values = {'author' : author.encode('utf-8'),
                   'comment' : comment.encode('utf-8'),
                   'ticketid' : ticket.id,
@@ -37,11 +45,29 @@ class LikeBackPlugin(Component):
                   'status' : ticket['status'].encode('utf-8'),
                   'resolution' : ticket['resolution'].encode('utf-8'),
                   'secret' : integration_secret.encode('utf-8')}
-        data = urllib.urlencode( values )
-        req = urllib2.Request( likeback_url, data )
-        response = urllib2.urlopen( req )
-        response = response.read()
-        print response
+
+        # Send the signal
+        try:
+            data = urllib.urlencode( values )
+            req = urllib2.Request( likeback_url, data )
+            response = urllib2.urlopen( req )
+            response = response.read()
+            print response
+        except HTTPError, e:
+            if hasattr( e, 'code' ):
+                error_code = e.code
+            else:
+                error_code = "404"
+
+            if hasattr( e, 'reason' ):
+                error_reason = e.reason
+            else:
+                error_reason = "Not Found!"
+
+            self._die( 'net_error', "Got '%s: %s' error while loading '%s'" % ( error_code, error_reason, likeback_url ) )
+        except ValueError:
+            self._die( 'net_error', "The LikeBack URL is not valid: '%s'" % likeback_url )
+
 
     def ticket_created(self, ticket):
         """Called when a ticket is created.
@@ -52,3 +78,16 @@ class LikeBackPlugin(Component):
         """Called when a ticket is deleted.
         Don't do anything here, we don't care.
         """
+
+    def _die(self, error, extrainfo ):
+        """Outputs an error.
+        """
+        if error == 'no_conf':
+            self.log.info( "LikeBack plugin configuration variable '%s' not found!" % extrainfo )
+            raise TracError( "The LikeBack plugin is not correctly configured! The '%s' variable is missing. Please re-read the README file." % extrainfo )
+        elif error == 'net_error':
+            self.log.info( "Network error occurred in the LikeBack plugin. %s" % extrainfo )
+            raise TracError( "The LikeBack plugin could not load its ticket updating system. %s" % extrainfo )
+        else:
+            self.log.info( "Unknown error occurred in the LikeBack plugin." )
+            raise TracError( "Unknown Error! The LikeBack Plugin has encountered an unspecified error!" )
