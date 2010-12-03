33515177f0c9205157d34fe31a32337dc8461468
Harald Sitter <sitter.harald@gmail.com>
2010-12-03 22:26:17 +0100

    drop pointless pointers && license

--- src/likebackbar.cpp
+++ src/likebackbar.cpp
@@ -1,21 +1,25 @@
-/***************************************************************************
-                              likebackbar.cpp
-                             -------------------
-    begin                : unknown
-    imported to LB svn   : 3 june, 2009
-    copyright            : (C) 2006 by Sebastien Laout
-                           (C) 2008-2009 by Valerio Pilo, Sjors Gielen
-    email                : sjors@kmess.org
- ***************************************************************************/
+/*
+    Copyright © 2006 Sebastien Laout
+    Copyright © 2008-2009 Valerio Pilo
+    Copyright © 2008-2009 Sjors Gielen
+    Copyright © 2010 Harald Sitter <apachelogger@ubuntu.com>
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "likebackbar.h"
 
@@ -35,11 +39,7 @@ class LikeBackBarPrivate
 {
 public:
     LikeBackBarPrivate() :
-        connected(false),
-        likeButton(0),
-        dislikeButton(0),
-        bugButton(0),
-        featureButton(0)
+        connected(false)
     {}
     ~LikeBackBarPrivate() {}
 
@@ -47,11 +47,6 @@ public:
     bool connected;
     // The parent LikeBack instance
     LikeBack *likeBack;
-
-    QToolButton *likeButton;
-    QToolButton *dislikeButton;
-    QToolButton *bugButton;
-    QToolButton *featureButton;
 };
 
 // --------------------------------- Public --------------------------------- //
@@ -66,39 +61,39 @@ LikeBackBar::LikeBackBar(LikeBack *likeBack) :
     QHBoxLayout *layout = new QHBoxLayout(this);
     setLayout(layout);
 
-    d->likeButton = new QToolButton(this);
-    d->likeButton->setAutoRaise(true);
-    layout->addWidget(d->likeButton);
-    connect(d->likeButton, SIGNAL(clicked()), this, SLOT(likeClicked()));
+    QToolButton *likeButton = new QToolButton(this);
+    likeButton->setAutoRaise(true);
+    layout->addWidget(likeButton);
+    connect(likeButton, SIGNAL(clicked()), this, SLOT(likeClicked()));
 
-    d->dislikeButton = new QToolButton(this);
-    d->dislikeButton->setAutoRaise(true);
-    layout->addWidget(d->dislikeButton);
-    connect(d->dislikeButton, SIGNAL(clicked()), this, SLOT(dislikeClicked()));
+    QToolButton *dislikeButton = new QToolButton(this);
+    dislikeButton->setAutoRaise(true);
+    layout->addWidget(dislikeButton);
+    connect(dislikeButton, SIGNAL(clicked()), this, SLOT(dislikeClicked()));
 
-    d->bugButton = new QToolButton(this);
-    d->bugButton->setAutoRaise(true);
-    layout->addWidget(d->bugButton);
-    connect(d->bugButton, SIGNAL(clicked()), this, SLOT(bugClicked()));
+    QToolButton *bugButton = new QToolButton(this);
+    bugButton->setAutoRaise(true);
+    layout->addWidget(bugButton);
+    connect(bugButton, SIGNAL(clicked()), this, SLOT(bugClicked()));
 
-    d->featureButton = new QToolButton(this);
-    d->featureButton->setAutoRaise(true);
-    layout->addWidget(d->featureButton);
-    connect(d->featureButton, SIGNAL(clicked()), this, SLOT(featureClicked()));
+    QToolButton *featureButton = new QToolButton(this);
+    featureButton->setAutoRaise(true);
+    layout->addWidget(featureButton);
+    connect(featureButton, SIGNAL(clicked()), this, SLOT(featureClicked()));
 
     resize(sizeHint());
     setObjectName("LikeBackBar");
 
-    d->likeButton->setIcon(KIcon("edit-like-likeback"));
-    d->dislikeButton->setIcon(KIcon("edit-dislike-likeback"));
-    d->bugButton->setIcon(KIcon("tools-report-bug-likeback"));
-    d->featureButton->setIcon(KIcon("tools-report-feature-likeback"));
+    likeButton->setIcon(KIcon("edit-like-likeback"));
+    dislikeButton->setIcon(KIcon("edit-dislike-likeback"));
+    bugButton->setIcon(KIcon("tools-report-bug-likeback"));
+    featureButton->setIcon(KIcon("tools-report-feature-likeback"));
 
     LikeBack::ButtonCodes buttons = likeBack->buttons();
-    d->likeButton->setShown(buttons & LikeBack::Like);
-    d->dislikeButton->setShown(buttons & LikeBack::Dislike);
-    d->bugButton->setShown(buttons & LikeBack::Bug);
-    d->featureButton->setShown(buttons & LikeBack::Feature);
+    likeButton->setShown(buttons & LikeBack::Like);
+    dislikeButton->setShown(buttons & LikeBack::Dislike);
+    bugButton->setShown(buttons & LikeBack::Bug);
+    featureButton->setShown(buttons & LikeBack::Feature);
 
     kDebug(likeBackDebugArea()) << "CREATED.";
 }
