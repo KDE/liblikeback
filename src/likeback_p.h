4abe5dd3c3108200af278d501b02518161c5e397
Harald Sitter <sitter@kde.org>
2010-12-31 12:37:47 +0100

    header cleanup

--- src/likeback_p.h
+++ src/likeback_p.h
@@ -1,21 +1,25 @@
-/***************************************************************************
-                              likeback_p.h
-                             -------------------
-    begin                : unknown
-    imported to LB svn   : 3 june, 2009
-    copyright            : (C) 2006 by Sebastien Laout
-                           (C) 2008-2009 by Valerio Pilo, Sjors Gielen
-    email                : sjors@kmess.org
- ***************************************************************************/
+/*
+    Copyright © 2006 Sebastien Laout
+    Copyright © 2008-2009 Valerio Pilo
+    Copyright © 2008-2009 Sjors Gielen
+    Copyright © 2010 Harald Sitter <apachelogger@ubuntu.com>
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef LIKEBACK_PRIVATE_H
 #define LIKEBACK_PRIVATE_H
